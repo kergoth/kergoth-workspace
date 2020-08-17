d163dccbcac162adfa867b1ec3ac1f528bb29e71
Christopher Larson <kergoth@gmail.com>
2020-08-17 12:27:10 -0700

    contrib: add attach-workspace-local example
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- contrib/example/env.zsh
+++ contrib/example/env.zsh
@@ -2,3 +2,4 @@ export WORKSPACE_PROJECT_ROOT="$HOME/mel"
 export WORKSPACE_PROJECT_PATTERN="*/*"
 export WORKSPACE_DECORATE="${0:h}/decorate-workspace"
 export WORKSPACE_UNDECORATE="${0:h}/undecorate-workspace"
+export WORKSPACE_ATTACH=attach-workspace-local
