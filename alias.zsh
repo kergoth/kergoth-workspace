820a260a21c086609ba551e111e9c025cf0245fc
Christopher Larson <chris_larson@mentor.com>
2022-06-14 16:19:57 +0000

    Add initial config
    Signed-off-by: Christopher Larson <chris_larson@mentor.com>

---
+++ alias.zsh
@@ -0,0 +1,4 @@
+alias atw=attach-workspace
+alias nw=new-workspace
+alias wp=workspace-picker
+alias wpa="workspace-picker -d -a"
---
+++ env.zsh
@@ -0,0 +1,5 @@
+export WORKSPACE_PROJECT_ROOT="$HOME/mel"
+export WORKSPACE_PROJECT_PATTERN="*/*"
+export WORKSPACE_DECORATE="${0:h}/decorate-workspace"
+export WORKSPACE_UNDECORATE="${0:h}/undecorate-workspace"
+export WORKSPACE_ATTACH=attach-workspace-local
